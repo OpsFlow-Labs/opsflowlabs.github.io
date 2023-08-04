e8b80479ce35f96f7102f8db66dd5549081761df
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2023-08-04 13:53:07 -0400

    add target

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -195,6 +195,7 @@ export default function Footer() {
                 <li className="ml-4">
                   <Link
                     href="https://github.com/OpsFlow-Labs"
+                    target="_blank"
                     className="flex justify-center items-center text-purple-600 bg-gray-800 hover:text-gray-100 hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
                     aria-label="Github"
                   >
