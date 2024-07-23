f6d3d96743b54ef539a3866c6c538277bb2198a3
Kristina Orolic <kristinaorolic@gmail.com>
2024-07-23 21:32:38 +0200

    Formatting text

--- components/features.tsx
+++ components/features.tsx
@@ -73,29 +73,29 @@ export default function Features() {
                   d="M5.5 20A5.5 5.5 0 0 1 0 14.5 5.5 5.5 0 0 1 5.5 9 6.52 6.52 0 0 1 18 11.03l.5-.03c2.5 0 4.5 2 4.5 4.5S21 20 18.5 20h-13m0-10C3 10 1 12 1 14.5S3 19 5.5 19h13a3.5 3.5 0 0 0 3.5-3.5 3.5 3.5 0 0 0-3.5-3.5c-.56 0-1.1.13-1.57.37.07-.28.07-.57.07-.87A5.5 5.5 0 0 0 11.5 6a5.51 5.51 0 0 0-5.31 4.05L5.5 10Z"
                 />
               </svg>
-              <h4 className="h4 mb-2 text-left">
+              <h4 className="h4 mb-2 text-center">
                 Transform Faster with DevOps Consulting
               </h4>
-              <p className="text-lg text-gray-400 text-left">
+              <p className="text-lg text-gray-400 text-center">
                 Ready to supercharge your digital journey?
               </p>
-              <p className="text-lg text-gray-400 text-left">
+              <p className="text-lg text-gray-400 text-center">
                 Our DevOps Consulting Services are designed to help you
                 accelerate your transformation. Partner with our experts to:
               </p>
-              {/* <ul> */}
-              <li className="text-lg text-gray-400 text-left">
-                Review: Evaluate your current DevOps practices.
-              </li>
-              <li className="text-lg text-gray-400 text-left">
-                Assess: Gauge your organization's maturity level.
-              </li>
-              <li className="text-lg text-gray-400 text-left">
-                Plan: Get a customized action plan for guaranteed success.
-              </li>
-              {/* </ul> */}
+              <ul>
+                <li className="text-lg text-gray-400 text-center">
+                  Review: Evaluate your current DevOps practices.
+                </li>
+                <li className="text-lg text-gray-400 text-center">
+                  Assess: Gauge your organization's maturity level.
+                </li>
+                <li className="text-lg text-gray-400 text-center">
+                  Plan: Get a customized action plan for guaranteed success.
+                </li>
+              </ul>
 
-              <p className="text-lg text-gray-400 text-left mt-4">
+              <p className="text-lg text-gray-400 text-center mt-2">
                 Let’s streamline your operations and drive growth together!
               </p>
             </div>
@@ -155,10 +155,10 @@ export default function Features() {
                   d="M3.5 8a5.5 5.5 0 1 1 8.6 4.55 9 9 0 0 1 5.9 8.18.75.75 0 0 1-1.5.04 7.5 7.5 0 0 0-15 0 .75.75 0 0 1-1.5-.04 9 9 0 0 1 5.9-8.18A5.5 5.5 0 0 1 3.5 8ZM9 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm8.29 4c-.15 0-.3.01-.43.03a.75.75 0 1 1-.22-1.48 4.53 4.53 0 0 1 3.38 8.1 6.69 6.69 0 0 1 3.96 6.1.75.75 0 0 1-1.5 0 5.2 5.2 0 0 0-3.7-4.97l-.53-.16v-1.68l.41-.2A3.03 3.03 0 0 0 17.29 8Z"
                 />
               </svg>
-              <h4 className="h4 mb-2 text-left">
+              <h4 className="h4 mb-2 text-center">
                 Exceptional Staffing Solutions for Every Need
               </h4>
-              <p className="text-lg text-gray-400 text-left">
+              <p className="text-lg text-gray-400 text-center">
                 Unlock the talent you need with our comprehensive Staffing
                 Services. Whether you require contract staffing, full-time
                 hires, or temporary to permanent placements, we’ve got you
@@ -233,10 +233,10 @@ export default function Features() {
                   d="M5 4h13a3 3 0 0 1 3 3v7a3 3 0 0 1-3 3h-4.5l.5 3h1v1H8v-1h1l.5-3H5a3 3 0 0 1-3-3V7a3 3 0 0 1 3-3m5.5 13-.5 3h3l-.5-3h-2M5 5a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h13a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H5Z"
                 />
               </svg>
-              <h4 className="h4 mb-2 text-left">
+              <h4 className="h4 mb-2 text-center">
                 Innovative Custom Development at Your Service
               </h4>
-              <p className="text-lg text-gray-400 text-left">
+              <p className="text-lg text-gray-400 text-center">
                 Our dedicated full-stack engineers bring years of experience to
                 the table, delivering fully-functional, scalable projects that
                 are always on-time and on-budget. Trust us to turn your vision
