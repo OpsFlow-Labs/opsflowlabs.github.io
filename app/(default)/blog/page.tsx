1a809f8b44fa8dae79fdd287ad6c29aee51d47fc
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2025-09-27 12:22:44 -0400

    add new case study

--- app/(default)/blog/page.tsx
+++ app/(default)/blog/page.tsx
@@ -16,7 +16,7 @@ export default function Blog() {
   });
   // Blog Post Number
   const featuredPost = allPosts[0];
-  const posts = allPosts.slice(0, 11);
+  const posts = allPosts.slice(0, 12);
 
   return (
     <>
