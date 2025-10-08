2c713e36f1833d596c568cb5c5a7bef855c1f67a
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2025-10-08 03:49:40 -0400

    add vault case study

--- app/(default)/blog/page.tsx
+++ app/(default)/blog/page.tsx
@@ -16,7 +16,7 @@ export default function Blog() {
   });
   // Blog Post Number
   const featuredPost = allPosts[0];
-  const posts = allPosts.slice(0, 13);
+  const posts = allPosts.slice(0, 14);
 
   return (
     <>
