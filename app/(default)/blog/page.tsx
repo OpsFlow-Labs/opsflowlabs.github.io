628d526bfc4f3f68c873373f93444bfb36e0b5e7
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2025-10-22 10:19:09 -0400

    add dynamic credential case study

--- app/(default)/blog/page.tsx
+++ app/(default)/blog/page.tsx
@@ -16,7 +16,7 @@ export default function Blog() {
   });
   // Blog Post Number
   const featuredPost = allPosts[0];
-  const posts = allPosts.slice(0, 16);
+  const posts = allPosts.slice(0, 17);
 
   return (
     <>
