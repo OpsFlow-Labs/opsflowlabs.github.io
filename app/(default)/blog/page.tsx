c0b9a91e0fd086f03a8d63b640dafd01d0ef4556
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2025-10-30 05:40:51 -0400

    add hybrid cloud case study

--- app/(default)/blog/page.tsx
+++ app/(default)/blog/page.tsx
@@ -16,7 +16,7 @@ export default function Blog() {
   });
   // Blog Post Number
   const featuredPost = allPosts[0];
-  const posts = allPosts.slice(0, 18);
+  const posts = allPosts.slice(0, 19);
 
   return (
     <>
