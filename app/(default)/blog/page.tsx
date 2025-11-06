18239c410fc45443de1ad054b255204c6fdd813a
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2025-11-06 11:41:51 -0500

    update number

--- app/(default)/blog/page.tsx
+++ app/(default)/blog/page.tsx
@@ -16,7 +16,7 @@ export default function Blog() {
   });
   // Blog Post Number
   const featuredPost = allPosts[0];
-  const posts = allPosts.slice(0, 20);
+  const posts = allPosts.slice(0, 21);
 
   return (
     <>
