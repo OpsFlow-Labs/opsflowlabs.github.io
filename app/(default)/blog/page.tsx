e0c21557ede251855aa843a6ad495ffc3ec89388
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2025-11-25 10:41:15 -0500

    add digital transformation case study

--- app/(default)/blog/page.tsx
+++ app/(default)/blog/page.tsx
@@ -16,7 +16,7 @@ export default function Blog() {
   });
   // Blog Post Number
   const featuredPost = allPosts[0];
-  const posts = allPosts.slice(0, 23);
+  const posts = allPosts.slice(0, 24);
 
   return (
     <>
